Update dependencies and fix latency calculation

multi_x_serverless/global_routing/internal/solver/playground.py
@@ -1,42 +1,15 @@
 import random
-from unittest.mock import MagicMock
-
+from unittest.mock import patch, MagicMock
 import networkx as nx
 import numpy as np
 
 from multi_x_serverless.global_routing.internal.solver.solver import find_viable_deployment_options
+from multi_x_serverless.global_routing.internal.solver.chalicelib.regions import get_regions
+from multi_x_serverless.global_routing.internal.solver.chalicelib.utils import DEFAULT_REGION, OPT_IN_REGIONS
 
 NUMBER_OF_REGIONS = 10
 NUMBER_OF_FUNCTIONS = 5
 
-# Mock the other functions of Solver
-get_cost_matrix = MagicMock(
-    return_value=np.array(
-        [[lambda x, y, z: random.randint(1, 10) for _ in range(NUMBER_OF_REGIONS)] for _ in range(NUMBER_OF_FUNCTIONS)]
-    )
-)
-
-get_egress_cost_matrix = MagicMock(
-    return_value=np.array([[random.randint(1, 10) for _ in range(NUMBER_OF_REGIONS)] for _ in range(NUMBER_OF_REGIONS)])
-)
-
-get_runtime_array = MagicMock(
-    return_value=np.array([lambda x, y, z: random.randint(1, 10) for _ in range(NUMBER_OF_REGIONS)])
-)
-
-get_latency_matrix = MagicMock(
-    return_value=np.array([[random.randint(1, 10) for _ in range(NUMBER_OF_REGIONS)] for _ in range(NUMBER_OF_REGIONS)])
-)
-
-get_execution_carbon_matrix = MagicMock(
-    return_value=np.array(
-        [[lambda x, y, z: random.randint(1, 10) for _ in range(NUMBER_OF_REGIONS)] for _ in range(NUMBER_OF_FUNCTIONS)]
-    )
-)
-
-get_transmission_carbon_matrix = MagicMock(
-    return_value=np.array([[random.randint(1, 10) for _ in range(NUMBER_OF_REGIONS)] for _ in range(NUMBER_OF_REGIONS)])
-)
 
 dag = nx.DiGraph()
 for i in range(NUMBER_OF_FUNCTIONS):
@@ -51,15 +24,13 @@ for node in range(NUMBER_OF_FUNCTIONS - 1):
 
 build_dag = MagicMock(return_value=dag)
 
-regions = np.array([(f"region_{i}", f"provider_{i}") for i in range(NUMBER_OF_REGIONS)])
-
-COST_CONSTRAINT = 300
-RUNTIME_CONSTRAINT = 300
-CARBON_CONSTRAINT = 300
+COST_CONSTRAINT = 100000
+RUNTIME_CONSTRAINT = 100000
+CARBON_CONSTRAINT = 100000
 
 workflow_description = {
-    "functions": [{"name": i, "some_other_attribute": "some_other_value"} for i in range(NUMBER_OF_FUNCTIONS)],
-    "start_hop": "region_0",
+    "functions": [{"name": i, } for i in range(NUMBER_OF_FUNCTIONS)],
+    "start_hop": DEFAULT_REGION,
     "constraints": {
         "cost": COST_CONSTRAINT,
         "runtime": RUNTIME_CONSTRAINT,
@@ -70,15 +41,23 @@ workflow_description = {
 function_runtime_measurements = {i: random.randint(1, 10) for i in range(NUMBER_OF_FUNCTIONS)}
 function_data_transfer_size_measurements = {i: random.randint(1, 10) for i in range(NUMBER_OF_FUNCTIONS)}
 
+print("Starting to find viable deployment options")
+
 # Call the find_viable_deployment_options function
-viable_options = find_viable_deployment_options(
-    regions, function_runtime_measurements, function_data_transfer_size_measurements, workflow_description
-)
+@patch("multi_x_serverless.global_routing.internal.solver.solver.get_dag")
+def test_find_viable_deployment_options(mock_get_dag: MagicMock) -> None:
+    regions = get_regions()
+    regions = [region for region in regions if region[0] not in OPT_IN_REGIONS]
+    regions = regions[:NUMBER_OF_REGIONS]
+    viable_options = find_viable_deployment_options(
+        regions, function_runtime_measurements, function_data_transfer_size_measurements, workflow_description
+    )
+    print(len(viable_options))
 
-print(len(viable_options))
+    # Assert the expected results
+    for option in viable_options:
+        assert option[1] <= COST_CONSTRAINT
+        assert option[2] <= RUNTIME_CONSTRAINT
+        assert option[3] <= CARBON_CONSTRAINT
 
-# Assert the expected results
-for option in viable_options:
-    assert option[1] <= COST_CONSTRAINT
-    assert option[2] <= RUNTIME_CONSTRAINT
-    assert option[3] <= CARBON_CONSTRAINT
+test_find_viable_deployment_options()