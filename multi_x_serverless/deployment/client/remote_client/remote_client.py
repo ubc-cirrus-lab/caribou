cbfc1542ceb16aa1c8d071c555f337ab07827206
vgsteiger <v.gsteiger@gmail.com>
2024-01-08 13:46:25 -0800

    Add support for merge functionality in MultiXServerlessWorkflow class

--- multi_x_serverless/deployment/client/remote_client/remote_client.py
+++ multi_x_serverless/deployment/client/remote_client/remote_client.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Optional
 
 from multi_x_serverless.deployment.client.deploy.models.resource import Resource
 
@@ -39,12 +39,45 @@ class RemoteClient(ABC):
     def update_role(self, role_name: str, policy: str, trust_policy: dict) -> str:
         raise NotImplementedError()
 
+    def invoke_function(
+        self,
+        message: str,
+        identifier: str,
+        workflow_instance_id: str,
+        merge: bool = False,
+        function_name: Optional[str] = None,
+        expected_counter: int = -1,
+    ) -> None:
+        if merge:
+            if function_name is None:
+                raise RuntimeError("Function name must be specified for merge")
+            if expected_counter == -1:
+                raise RuntimeError("Expected counter must be specified for merge")
+
+            self.upload_message_for_merge(function_name, workflow_instance_id, message)
+            counter = self.increment_counter(function_name, workflow_instance_id)
+
+            if counter != expected_counter:
+                return
+        try:
+            self.send_message_to_messaging_service(identifier, message)
+        except Exception as e:
+            raise RuntimeError("Could not invoke function through SNS") from e
+
     @abstractmethod
-    def invoke_function(self, message: str, region: str, identifier: str, merge: bool = False) -> None:
+    def send_message_to_messaging_service(self, identifier: str, message: str) -> None:
         raise NotImplementedError()
 
     @abstractmethod
     def get_predecessor_data(self, current_instance_name: str, workflow_instance_id: str) -> list[dict[str, Any]]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def increment_counter(self, function_name: str, workflow_instance_id: str) -> int:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def upload_message_for_merge(self, function_name: str, workflow_instance_id: str, message: str) -> None:
         raise NotImplementedError()
 
     @abstractmethod
