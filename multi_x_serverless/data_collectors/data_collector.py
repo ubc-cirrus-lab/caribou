a24b33b7ac0f46b6ed67ce206cdaf6c276535af3
Danidite <1099335751qq@gmail.com>
2024-01-25 15:57:42 -0800

    Added data_collector architectural image

--- multi_x_serverless/data_collectors/data_collector.py
+++ multi_x_serverless/data_collectors/data_collector.py
@@ -5,12 +5,10 @@ from typing import Any
 from multi_x_serverless.common.models.endpoints import Endpoints
 
 from multi_x_serverless.data_collectors.components.data_retriever import DataRetriever
-from multi_x_serverless.data_collectors.components.data_processor import DataProcessor
 from multi_x_serverless.data_collectors.components.data_exporter import DataExporter
 
 class DataCollector(ABC):
     _data_retriever: DataRetriever
-    _data_processor: DataProcessor
     _data_exporter: DataExporter
     _data_table: str
     
@@ -18,7 +16,6 @@ class DataCollector(ABC):
     def __init__(self) -> None:
         self._data_collector_client = Endpoints().get_data_collector_client()
 
+    @abstractmethod
     def run(self) -> None:
-        retrieved_data = self._data_retriever.collect_data()
-        processed_data = self._data_processor.process_data(retrieved_data)
-        self._data_exporter.export_data(processed_data)
+        raise NotImplementedError
