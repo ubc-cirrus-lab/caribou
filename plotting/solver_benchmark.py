52e33cbb247ae77495c51ddfadb501440b868aa6
vgsteiger <v.gsteiger@gmail.com>
2024-01-30 22:46:40 +0100

    Add fast solver benchmark plot

--- plotting/solver_benchmark.py
+++ plotting/solver_benchmark.py
@@ -8,13 +8,17 @@ import numpy as np
 import matplotlib.colors as mcolors
 
 
-def plot_single(ax1, ax2, data, x_key, y_left_key, y_right_key):
+def plot_single(ax1, ax2, data, x_key, y_left_key, y_right_key, only_fast=False):
     colormap = colormaps["viridis"]
     num_solvers = len(data)
     width = 0.2
     handles, labels = [], []
 
     for i, (solver, solver_data) in enumerate(data.items()):
+        if solver == "BFSFineGrainedSolver" and only_fast:
+            continue
+        if not only_fast:
+            solver_data = [d for d in solver_data if d["number of instances"] < 8 and d["number of regions"] < 8]
         ax1_data_dict = defaultdict(list)
         for d in solver_data:
             ax1_data_dict[d[x_key]].append(d[y_left_key])
@@ -56,13 +60,13 @@ def plot_single(ax1, ax2, data, x_key, y_left_key, y_right_key):
     fig.legend(handles, labels, loc="upper right")
 
 
-def plot_row(fig, axs, data, x_key, y_keys):
+def plot_row(fig, axs, data, x_key, y_keys, only_fast=False):
     for i, y_key in enumerate(y_keys):
         ax2 = axs[i].twinx()
-        plot_single(axs[i], ax2, data, x_key, y_key, "runtime")
+        plot_single(axs[i], ax2, data, x_key, y_key, "runtime", only_fast=only_fast)
 
 
-def plot_all(data):
+def plot_all(data, only_fast=False):
     x_keys = ["number of regions", "number of instances", "number of sync nodes"]
     y_keys = ["best cost", "best runtime", "best carbon"]
     fig, axs = plt.subplots(3, 3, figsize=(15, 15))
@@ -73,11 +77,15 @@ def plot_all(data):
             {solver: [d for d in solver_data if d[x_keys[i]]] for solver, solver_data in data.items()},
             x_keys[i],
             y_keys,
+            only_fast=only_fast,
         )
     fig.tight_layout(rect=[0, 0, 1, 0.97])  # Adjust layout to make room for legend
     # get this file location
     current_path = os.path.dirname(os.path.realpath(__file__))
-    plt.savefig(os.path.join(current_path, "plots", "solver_benchmark.pdf"))
+    if only_fast:
+        plt.savefig(os.path.join(current_path, "plots", "solver_benchmark_fast.pdf"))
+    else:
+        plt.savefig(os.path.join(current_path, "plots", "solver_benchmark.pdf"))
 
 
 def read_data(path_to_data):
@@ -90,4 +98,5 @@ def read_data(path_to_data):
 if __name__ == "__main__":
     path_to_data = sys.argv[1]
     data = read_data(path_to_data)
-    plot_all(data)
+    plot_all(data, only_fast=True)
+    plot_all(data, only_fast=False)
