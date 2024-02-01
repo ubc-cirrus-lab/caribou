3f31eb8ae9d63fc78bd8c0243d7c7c6f8bb0b6f1
vgsteiger <v.gsteiger@gmail.com>
2024-02-01 16:18:50 +0100

    Add timed update checker for deployment

--- multi_x_serverless/update_checkers/deployment_update_checker.py
+++ multi_x_serverless/update_checkers/deployment_update_checker.py
@@ -1,14 +1,16 @@
 from multi_x_serverless.update_checkers.update_checker import UpdateChecker
 from multi_x_serverless.common.constants import WORKFLOW_PLACEMENT_SOLVER_STAGING_AREA_TABLE
+from multi_x_serverless.deployment.server.main import run
+
 
 class DeploymentUpdateChecker(UpdateChecker):
+    # This should be a timed update checker (every hour)
     def __init__(self, name):
         super().__init__(name)
 
     def check(self) -> None:
-        updated_workflow_placements = self._endpoints.get_solver_workflow_placement_decision_client().get_keys(WORKFLOW_PLACEMENT_SOLVER_STAGING_AREA_TABLE)
-        if len(updated_workflow_placements) > 0:
-            print(f"Found {len(updated_workflow_placements)} updated workflow placements")
-            return False
-        else:
-            pass
+        updated_workflow_placements = self._endpoints.get_solver_workflow_placement_decision_client().get_keys(
+            WORKFLOW_PLACEMENT_SOLVER_STAGING_AREA_TABLE
+        )
+        for updated_workflow_placement in updated_workflow_placements:
+            run(updated_workflow_placement)
