d599e3afe71bfedd6df5eefe0087665d2dc4ba79
vgsteiger <v.gsteiger@gmail.com>
2024-02-01 08:24:24 +0100

    Refactor topological_sort method in DAG class

--- multi_x_serverless/tests/routing/models/test_dag.py
+++ multi_x_serverless/tests/routing/models/test_dag.py
@@ -2,9 +2,9 @@ import unittest
 import numpy as np
 
 from multi_x_serverless.routing.models.dag import DAG
+import time
 
 
-# TODO
 class TestDAG(unittest.TestCase):
     def setUp(self):
         self.nodes = [{"instance_name": "node1"}, {"instance_name": "node2"}, {"instance_name": "node3"}]
@@ -25,6 +25,76 @@ class TestDAG(unittest.TestCase):
         self.dag.add_edge("node4", "node5")
         expected_matrix = np.zeros((len(self.nodes), len(self.nodes)))
         np.testing.assert_array_equal(self.dag.get_adj_matrix(), expected_matrix)
+
+    def test_topological_sort(self):
+        self.dag.add_edge("node1", "node2")
+        self.dag.add_edge("node2", "node3")
+        self.dag.add_edge("node1", "node3")
+
+        self.assertEqual(self.dag.topological_sort(), [0, 1, 2])
+
+    def test_topological_sort_complex_1(self):
+        dag = DAG([{"instance_name": "node1"}, {"instance_name": "node2"}, {"instance_name": "node3"}, {"instance_name": "node4"}, {"instance_name": "node5"}])
+        dag.add_edge("node1", "node2")
+        dag.add_edge("node2", "node3")
+        dag.add_edge("node3", "node4")
+        dag.add_edge("node4", "node5")
+
+        self.assertEqual(dag.topological_sort(), [0, 1, 2, 3, 4])
+
+    def test_topological_sort_complex_2(self):
+        dag = DAG([{"instance_name": "node1"}, {"instance_name": "node2"}, {"instance_name": "node3"}, {"instance_name": "node4"}])
+        dag.add_edge("node1", "node2")
+        dag.add_edge("node1", "node3")
+        dag.add_edge("node2", "node4")
+        dag.add_edge("node3", "node4")
+
+        self.assertEqual(dag.topological_sort(), [0, 1, 2, 3])
+
+    def test_topological_sort_complex_3(self):
+        self.dag.add_edge("node1", "node2")
+        self.dag.add_edge("node2", "node3")
+        self.dag.add_edge("node3", "node1")
+
+        with self.assertRaises(Exception):
+            self.dag.topological_sort()
+
+    def test_topological_sort_complex_4(self):
+        dag = DAG([{"instance_name": "node1"}, {"instance_name": "node2"}, {"instance_name": "node3"}, {"instance_name": "node4"}])
+        dag.add_edge("node1", "node2")
+        dag.add_edge("node2", "node3")
+        dag.add_edge("node3", "node4")
+        dag.add_edge("node4", "node1")
+
+        with self.assertRaises(Exception):
+            dag.topological_sort()
+
+    def test_topological_sort_complex_5(self):
+        dag = DAG([{"instance_name": "node1"}, {"instance_name": "node2"}, {"instance_name": "node3"}, {"instance_name": "node4"}, {"instance_name": "node5"}, {"instance_name": "node6"}])
+        dag.add_edge("node1", "node2")
+        dag.add_edge("node2", "node3")
+        dag.add_edge("node3", "node4")
+        dag.add_edge("node4", "node5")
+        dag.add_edge("node5", "node6")
+
+        self.assertEqual(dag.topological_sort(), [0, 1, 2, 3, 4, 5])
+    
+    def test_topological_sort_complex_large(self):
+        nodes = [{"instance_name": f"node{i}"} for i in range(1, 1001)]
+        dag = DAG(nodes)
+
+        for i in range(1, 1000):
+            dag.add_edge(f"node{i}", f"node{i+1}")
+
+        self.assertEqual(dag.topological_sort(), list(range(1000)))
+
+    def test_topological_sort_cycle(self):
+        self.dag.add_edge("node1", "node2")
+        self.dag.add_edge("node2", "node3")
+        self.dag.add_edge("node3", "node1")
+
+        with self.assertRaises(ValueError):
+            self.dag.topological_sort()
 
 
 if __name__ == "__main__":
