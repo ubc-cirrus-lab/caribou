d0c9108cbbbc9dd6a33d6b6b45001fa37c7f5446
Danidite <1099335751qq@gmail.com>
2024-01-31 21:30:34 -0800

    removed incompleted changes

--- multi_x_serverless/data_collector/components/workflow/workflow_collector.py
+++ multi_x_serverless/data_collector/components/workflow/workflow_collector.py
@@ -38,27 +38,3 @@ class WorkflowCollector(DataCollector):
         )
 
         # For workflow collector, no need to modify the time stamp of the regions
-
-
-class CarbonCollector(DataCollector):
-    def __init__(self, config: dict) -> None:
-        super().__init__()
-        self._data_collector_name: str = "carbon_collector"
-
-        carbon_region_table: str = CARBON_REGION_TABLE
-
-        self._data_retriever: CarbonRetriever = CarbonRetriever(self._data_collector_client, config)
-        self._data_exporter: CarbonExporter = CarbonExporter(self._data_collector_client, carbon_region_table)
-
-    def run(self) -> None:
-        # Retrieve available regions
-        self._available_region_data = self._data_retriever.retrieve_available_regions()
-
-        carbon_region_data: dict[str, Any] = self._data_retriever.retrieve_carbon_region_data()
-
-        self._data_exporter.export_all_data(carbon_region_data)
-
-        # Updates the timestamp of modified regions
-        modified_regions: set[str] = self._data_exporter.get_modified_regions()
-        # Important: Regions are stored as provider_region
-        self._data_exporter.update_available_region_timestamp(self._data_collector_name, modified_regions)
