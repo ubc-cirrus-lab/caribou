8d7a8629dd971cb01d375e49dce4a364742712ec
vgsteiger <v.gsteiger@gmail.com>
2024-02-07 09:23:17 -0800

    Add fifth function and modify existing functions

--- multi_x_serverless/deployment/client/cli/template/app.py
+++ multi_x_serverless/deployment/client/cli/template/app.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from multi_x_serverless.deployment.client import MultiXServerlessWorkflow
+from {{ workflow_name }}.src.fifth import fifth
 
 workflow = MultiXServerlessWorkflow(name="{{ workflow_name }}", version="0.0.1")
 
@@ -33,102 +34,101 @@ workflow = MultiXServerlessWorkflow(name="{{ workflow_name }}", version="0.0.1")
     environment_variables=[{"key": "example_key", "value": "example_value"}],
 )
 def first_function(event: dict[str, Any]) -> dict[str, Any]:
-    payload = {
-        "hello": "world",
-    }
+    print("Hello from first function")
+
+    payload = "First Function says hello to second function"
 
     workflow.invoke_serverless_function(second_function, payload)
 
-    payload = {
-        "hello": "world2",
-    }
+    payload = "First Function says hello to third function"
 
     workflow.invoke_serverless_function(third_function, payload)
 
-    payload = {
-        "hello": "world3",
-    }
+    payload = "First Function says hello to sixth function"
 
     workflow.invoke_serverless_function(sixth_function, payload)
 
+    print("Goodbye from first function")
     return {"status": 200}
 
 
 @workflow.serverless_function(name="Second-Function")
 def second_function(event: dict[str, Any]) -> dict[str, Any]:
-    request = event["hello"]
+    print("Hello from second function")
 
-    print(request)
+    print(event)
 
     payload = {
-        "hello": "world4",
+        "hello": "Second Function says hello to fifth function",
     }
 
     workflow.invoke_serverless_function(fifth_function, payload)
 
+    print("Goodbye from second function")
     return {"status": 200}
 
 
 @workflow.serverless_function(name="Third-Function")
 def third_function(event: dict[str, Any]) -> dict[str, Any]:
-    request = event["hello"]
+    print("Hello from third function")
 
-    print(request)
+    print(event)
 
     payload = {
-        "hello": "world5",
+        "hello": "Third Function says hello to fourth function",
     }
 
     workflow.invoke_serverless_function(fourth_function, payload)
 
+    print("Goodbye from third function")
     return {"status": 200}
 
 
 @workflow.serverless_function(name="Fourth-Function")
 def fourth_function(event: dict[str, Any]) -> dict[str, Any]:
+    print("Hello from fourth function")
     request = event["hello"]
 
     print(request)
 
     payload = {
-        "hello": "world6",
+        "hello": "Fourth Function says hello to fifth function",
     }
 
     workflow.invoke_serverless_function(fifth_function, payload)
 
+    print("Goodbye from fourth function")
     return {"status": 200}
 
 
 @workflow.serverless_function(name="Fifth-Function")
 def fifth_function(event: dict[str, Any]) -> dict[str, Any]:
-    responses: list[dict[str, Any]] = workflow.get_predecessor_data()
-
-    for response in responses:
-        print(response["hello"])
-
-    return {"status": 200}
+    return fifth(workflow, event, seventh_function)
 
 
 @workflow.serverless_function(name="Sixth-Function")
 def sixth_function(event: dict[str, Any]) -> dict[str, Any]:
-    request = event["hello"]
+    print("Hello from sixth function")
 
-    print(request)
+    print(event)
 
     payload = {
-        "hello": "world7",
+        "hello": "Sixth Function says hello to seventh function",
     }
 
     workflow.invoke_serverless_function(seventh_function, payload)
 
+    print("Goodbye from sixth function")
     return {"status": 200}
 
 
 @workflow.serverless_function(name="Seventh-Function")
 def seventh_function(event: dict[str, Any]) -> dict[str, Any]:
+    print("Hello from seventh function")
     responses: list[dict[str, Any]] = workflow.get_predecessor_data()
 
     for response in responses:
         print(response["hello"])
 
+    print("Goodbye from seventh function")
     return {"status": 200}
---
+++ multi_x_serverless/deployment/client/cli/template/src/fifth.py
@@ -0,0 +1,15 @@
+from multi_x_serverless.deployment.client import MultiXServerlessWorkflow
+from typing import Any
+
+
+def fifth(workflow: MultiXServerlessWorkflow, event: dict[str, Any], seventh_function: Any) -> dict[str, Any]:
+    print("Hello from fifth function")
+    responses: list[dict[str, Any]] = workflow.get_predecessor_data()
+
+    for response in responses:
+        print(response["hello"])
+
+    workflow.invoke_serverless_function(seventh_function, {"hello": "Fifth Function says hello to seventh function"})
+
+    print("Goodbye from fifth function")
+    return {"status": 200}
