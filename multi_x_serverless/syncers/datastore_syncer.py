84ca2d78bc8907fcc9ffc3f940f700cdd6533653
vgsteiger <v.gsteiger@gmail.com>
2024-02-10 10:19:49 -0800

    Added DatastoreSyncer class

--- multi_x_serverless/syncers/datastore_syncer.py
+++ multi_x_serverless/syncers/datastore_syncer.py
@@ -1,1 +1,25 @@
-# TODO (#112): Implement DatastoreSyncer
+from multi_x_serverless.common.models.endpoints import Endpoints
+from multi_x_serverless.common.constants import (
+    DEPLOYMENT_MANAGER_RESOURCE_TABLE,
+)
+
+
+class DatastoreSyncer:
+    def __init__(self):
+        self.endpoints = Endpoints()
+
+    def sync(self):
+        currently_deployed_workflows = self.endpoints.get_deployment_manager_client().get_keys(
+            DEPLOYMENT_MANAGER_RESOURCE_TABLE
+        )
+
+        for workflow in currently_deployed_workflows:
+            # Get deployed regions
+
+            # For each deployed region, get the cloudsource logs
+
+            # Summarize the logs since the last sync
+
+            # Upload the summarized logs to the datastore
+
+            pass
