b82847ac47ad1a0974560f1c1b608c8a9cb81832
Danidite <1099335751qq@gmail.com>
2024-02-19 11:53:05 -0800

    Additional progress, make seperate distribution with proper compliance.

--- multi_x_serverless/routing/distribution_solver/data_type/distribution.py
+++ multi_x_serverless/routing/distribution_solver/data_type/distribution.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import TypeVar
 
 import numpy as np
@@ -5,77 +6,47 @@ import numpy as np
 T = TypeVar("T", bound="Distribution")
 
 
-class Distribution:
-    _samples: np.array[float]
-    _max_sample_size: int
+class Distribution(ABC):
+    _samples: np.ndarray
 
-    def __init__(self, samples: np.array[float], max_sample_size: int = 1000):
-        self._samples = samples
-        self._max_sample_size = max_sample_size
-    
-    def get_mean(self) -> float:
-        return np.mean(self._samples)
-    
-    def get_median(self) -> float:
-        return np.median(self._samples)
-    
-    def get_percentile(self, percentile: int) -> float:
-        return np.percentile(self._samples, percentile)
+    def __init__(self) -> None:
+        self._tail_latency_threshold = 95  # Would become an environemntal variable later
 
-    def get_merged_distribution(self, parent_distributions: list[T], max_sample_size: int = -1) -> T:
-        max_sample_size = self._consider_sample_size(max_sample_size)
+    @abstractmethod
+    def get_percentile(self, percentile: int, ignore_zeros: bool = False) -> float:
+        raise NotImplementedError
 
-        # Here we should combine the distributions in a merged manner
-        # Current strategy: Find the longest (maximum) runtime across these arrays for each corresponding index
-        # Perhaps we should consider more sophisticated methods in the future
+    @abstractmethod
+    def get_merged_distribution(self, parent_distributions: list[T]) -> T:
+        raise NotImplementedError
 
-        # First we must extract the samples from the parent distributions
-        parent_runtimes = [distribution._samples for distribution in parent_distributions]
+    @abstractmethod
+    def _combine_sequential(self, distribution1: T, distribution2: T) -> T:
+        raise NotImplementedError
 
-        # Then we have the find the minimum sample size of those runtimes and
-        # subsample the longer ones such that they are all the same size
-        min_sample_size = min([len(samples) for samples in parent_runtimes])
-
-        # Then since they are all in the same size we can convert them to a numpy array
-        parent_runtimes = np.array([self._downsample(samples, min_sample_size) for samples in parent_runtimes])
-
-        # Finally we can find the maximum runtime for each index using np.maximum.reduce
-        max_runtimes = np.maximum.reduce(parent_runtimes)
-
-        # Return the downsampled version of the max runtimes and convert it to a Distribution object
-        return T(self._downsample(max_runtimes, max_sample_size), max_sample_size)
-
-    def _combine_sequential(self, distribution1: T, distribution2: T, max_sample_size: int = -1) -> T:
-        max_sample_size = self._consider_sample_size(max_sample_size)
-
-        # Here we should combine the two distributions in a sequential manner
-        # Current strategy: Effectively calculates the total runtime for every possible pair of addition operations from the two sequences.
-        combined_samples = np.add.outer(distribution1._samples, distribution2._samples).flatten()
-
-        # Return the downsampled version of the combined samples and convert it to a Distribution object
-        return T(self._downsample(combined_samples, max_sample_size), max_sample_size)
-
-    def _downsample(self, samples: np.array[float], max_sample_size: int = -1) -> np.array[float]:
-        max_sample_size = self._consider_sample_size(max_sample_size)
-
-        if len(samples) <= max_sample_size:
-            return samples
-
-        # Here we should downsample the samples to the max_sample_size
-        # Current strategy: Perform simple random sampling without replacement, correctly handling the dataset size
-        # Perhaps we should consider more sophisticated methods in the future
-        return np.random.choice(samples, size=max_sample_size, replace=False)
-
-    def _consider_sample_size(self, sample_size: int) -> int:
-        if sample_size <= 0:  # Default sample sizes
-            sample_size = self._max_sample_size
-
-        return sample_size
-
+    @abstractmethod
     def __add__(self, other: T) -> T:
-        if isinstance(other, T):
-            return self._combine_sequential(self, other)
-        else:
-            raise TypeError(
-                "Unsupported operand type for +: '{}' and '{}'".format(type(self).__name__, type(other).__name__)
-            )
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_mean(self, ignore_zeros: bool = False) -> float:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_median(self, ignore_zeros: bool = False) -> float:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_tail_latency(self, ignore_zeros: bool = False) -> float:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_min(self, ignore_zeros: bool = False) -> float:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_max(self, ignore_zeros: bool = False) -> float:
+        raise NotImplementedError
+
+    def get_samples(self) -> np.ndarray:
+        return self._samples
--- multi_x_serverless/routing/distribution_solver/input/distribution_input_manager.py
+++ multi_x_serverless/routing/distribution_solver/input/distribution_input_manager.py
@@ -1,7 +1,11 @@
 # Loader
+import numpy as np
+
 from multi_x_serverless.routing.distribution_solver.data_type.distribution import Distribution
+from multi_x_serverless.routing.distribution_solver.data_type.sample_based_distribution import SampleBasedDistribution
 from multi_x_serverless.routing.models.dag import DAG
 from multi_x_serverless.routing.models.region import Region
+from multi_x_serverless.routing.workflow_config import WorkflowConfig
 
 
 class DistributionInputManager:  # pylint: disable=too-many-instance-attributes
@@ -9,8 +13,13 @@ class DistributionInputManager:  # pylint: disable=too-many-instance-attributes
     _region_indexer: Region
     _instance_indexer: DAG
 
-    def __init__(self) -> None:
+    def __init__(self, config: WorkflowConfig, setup_region_viability: bool = True) -> None:
         super().__init__()
+        self._config = config
+
+        # Setup the viability loader and load available regions
+        if setup_region_viability:
+            pass  # Not implemented
 
     def setup(self, regions_indexer: Region, instance_indexer: DAG) -> None:
         self._region_indexer = regions_indexer
@@ -21,14 +30,22 @@ class DistributionInputManager:  # pylint: disable=too-many-instance-attributes
     ) -> list[Distribution]:
         test_sum = instance_index + region_index
         print(test_sum)
-        return [Distribution(), Distribution(), Distribution()]
+        return [
+            SampleBasedDistribution(np.empty((0,))),
+            SampleBasedDistribution(np.empty((0,))),
+            SampleBasedDistribution(np.empty((0,))),
+        ]
 
     def get_transmission_cost_carbon_runtime_distribution(
         self, from_instance_index: int, to_instance_index: int, from_region_index: int, to_region_index: int
     ) -> list[Distribution]:
         test_sum = from_instance_index + to_instance_index + from_region_index + to_region_index
         print(test_sum)
-        return [Distribution(), Distribution(), Distribution()]
+        return [
+            SampleBasedDistribution(np.empty((0,))),
+            SampleBasedDistribution(np.empty((0,))),
+            SampleBasedDistribution(np.empty((0,))),
+        ]
 
     def get_all_regions(self) -> list[str]:
         return self._available_regions
