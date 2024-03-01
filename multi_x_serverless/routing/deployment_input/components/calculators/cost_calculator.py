6dab7477890259b94026b95883e7c1fa8d277844
Danidite <1099335751qq@gmail.com>
2024-03-01 12:10:38 -0800

    Initial implementation of distribution input

---
+++ multi_x_serverless/routing/deployment_input/components/calculators/cost_calculator.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+from multi_x_serverless.routing.deployment_input.components.calculator import InputCalculator
+from multi_x_serverless.routing.deployment_input.components.calculators.runtime_calculator import RuntimeCalculator
+from multi_x_serverless.routing.deployment_input.components.loaders.datacenter_loader import DatacenterLoader
+from multi_x_serverless.routing.deployment_input.components.loaders.workflow_loader import WorkflowLoader
+
+
+class CostCalculator(InputCalculator):
+    def __init__(
+        self,
+        datacenter_loader: DatacenterLoader,
+        workflow_loader: WorkflowLoader,
+        runtime_calculator: RuntimeCalculator,
+    ) -> None:
+        super().__init__()
+        self._datacenter_loader: DatacenterLoader = datacenter_loader
+        self._workflow_loader: WorkflowLoader = workflow_loader
+        self._runtime_calculator: RuntimeCalculator = runtime_calculator
+
+    def calculate_execution_cost_distribution(self, instance_name: str, region_name: str) -> np.ndarray:
+        # Get the runtime of the instance in the given region (s)
+        runtime_distribution: np.ndarray = self._runtime_calculator.calculate_runtime_distribution(
+            instance_name, region_name
+        )
+
+        # Get the number of vCPUs and Memory of the instance
+        provider, _ = region_name.split(":")
+        vcpu: float = self._workflow_loader.get_vcpu(instance_name, provider)
+        memory: float = self._workflow_loader.get_memory(instance_name, provider)
+
+        ## datacenter loader data
+        architecture: str = self._workflow_loader.get_architecture(instance_name, provider)
+        compute_cost: float = self._datacenter_loader.get_compute_cost(region_name, architecture)
+        invocation_cost: float = self._datacenter_loader.get_invocation_cost(region_name, architecture)
+
+        # Compute cost in USD /  GB-seconds
+        # Memory in MB, execution_time in seconds, vcpu in vcpu
+        memory_gb: float = memory / 1024
+        gbs: np.ndarray = memory_gb * vcpu * runtime_distribution
+
+        cost_from_compute: np.ndarray = compute_cost * gbs  # Element wise multiplication
+
+        # Invocation cost is simply cost per invocation
+        cost_from_invocation: float = invocation_cost
+
+        total_execution_cost: np.ndarray = cost_from_compute + cost_from_invocation  # Element wise addition
+        return total_execution_cost  # Already a numpy array
+
+    def calculate_transmission_cost_distribution(
+        self, from_instance_name: str, to_instance_name: str, from_region_name: str, to_region_name: str
+    ) -> np.ndarray:
+        # Get the data transfer size from the workflow loader (In units of GB)
+        transmission_size: np.ndarray = np.array(
+            self._workflow_loader.get_data_transfer_size_distribution(from_instance_name, to_instance_name)
+        )
+
+        # Get the providers of the 2 instances
+        from_provider, _ = from_region_name.split(":")
+        to_provider, _ = to_region_name.split(":")
+
+        # Determine if the transfer is intra-provider or inter-provider
+        intra_provider_transfer: bool = False
+        if from_provider == to_provider:
+            intra_provider_transfer = True
+
+        # Get the cost of transmission
+        transmission_cost_gb: float = self._datacenter_loader.get_transmission_cost(
+            to_provider, intra_provider_transfer
+        )
+
+        # Both in units of gb
+        transmission_cost: np.ndarray = transmission_size * transmission_cost_gb  # Element wise multiplication
+
+        return transmission_cost  # Already a numpy array
---
+++ multi_x_serverless/routing/deployment_input/components/loaders/workflow_loader.py
@@ -0,0 +1,129 @@
+from typing import Any, Optional
+
+from multi_x_serverless.common.constants import (
+    SOLVER_INPUT_ARCHITECTURE_DEFAULT,
+    SOLVER_INPUT_DATA_TRANSFER_SIZE_DEFAULT,
+    SOLVER_INPUT_INVOCATION_PROBABILITY_DEFAULT,
+    SOLVER_INPUT_PROJECTED_MONTHLY_INVOCATIONS_DEFAULT,
+    SOLVER_INPUT_VCPU_DEFAULT,
+    WORKFLOW_INSTANCE_TABLE,
+)
+from multi_x_serverless.common.models.remote_client.remote_client import RemoteClient
+from multi_x_serverless.common.provider import Provider
+from multi_x_serverless.routing.deployment_input.components.loader import InputLoader
+from multi_x_serverless.routing.workflow_config import WorkflowConfig
+
+
+class WorkflowLoader(InputLoader):
+    _workflow_data: dict[str, Any]
+    _instances_regions_and_providers: dict[str, Any]
+    _home_region: str
+
+    def __init__(self, client: RemoteClient, workflow_config: WorkflowConfig) -> None:
+        super().__init__(client, WORKFLOW_INSTANCE_TABLE)
+
+        # Parse the workflow config to get the instances, regions, and providers
+        ## Get the starthop or home region for the workflow
+        self._home_region: str = workflow_config.start_hops
+
+        ## Get the enabled providers for each instance
+        self._instances_regions_and_providers = {}
+        instances_data: list[dict] = workflow_config.instances
+        for instance in instances_data:
+            providers = instance.get("regions_and_providers", {}).get("providers", {})
+            self._instances_regions_and_providers[instance["instance_name"]] = providers
+
+    def setup(self, workflow_id: str) -> None:
+        self._workflow_data = self._retrieve_workflow_data(workflow_id)
+
+    def get_home_region(self) -> str:
+        return self._home_region
+
+    def get_runtime_distribution(self, instance_name: str, region_name: str) -> list[float]:
+        return (
+            self._workflow_data.get(instance_name, {})
+            .get("execution_summary", {})
+            .get(region_name, {})
+            .get("runtime_distribution", [])
+        )
+
+    def get_latency_distribution(
+        self,
+        from_instance_name: str,
+        to_instance_name: str,
+        from_region_name: str,
+        to_region_name: str,
+    ) -> list[float]:
+        return (
+            self._workflow_data.get(from_instance_name, {})
+            .get("invocation_summary", {})
+            .get(to_instance_name, {})
+            .get("transmission_summary", {})
+            .get(from_region_name, {})
+            .get(to_region_name, {})
+            .get("latency_distribution", [])
+        )
+
+    def get_data_transfer_size_distribution(self, from_instance_name: str, to_instance_name: str) -> list[float]:
+        return (
+            self._workflow_data.get(from_instance_name, {})
+            .get("invocation_summary", {})
+            .get(to_instance_name, {})
+            .get("data_transfer_size_distribution", [SOLVER_INPUT_DATA_TRANSFER_SIZE_DEFAULT])
+        )
+
+    def get_invocation_probability(self, from_instance_name: str, to_instance_name: str) -> float:
+        if from_instance_name == to_instance_name:  # Special case for start node
+            return 1
+
+        return (
+            self._workflow_data.get(from_instance_name, {})
+            .get("invocation_summary", {})
+            .get(to_instance_name, {})
+            .get("probability_of_invocation", SOLVER_INPUT_INVOCATION_PROBABILITY_DEFAULT)
+        )
+
+    def get_projected_monthly_invocations(self, instance_name: str) -> float:
+        return self._workflow_data.get(instance_name, {}).get(
+            "projected_monthly_invocations", SOLVER_INPUT_PROJECTED_MONTHLY_INVOCATIONS_DEFAULT
+        )
+
+    def get_vcpu(self, instance_name: str, provider_name: str) -> float:
+        vcpu = (
+            self._instances_regions_and_providers.get(instance_name, {})
+            .get(provider_name, {})
+            .get("config", {})
+            .get("vcpu", SOLVER_INPUT_VCPU_DEFAULT)
+        )
+
+        if vcpu < 0:
+            # Configure memory and vcpu configuration and or translation
+            if provider_name == Provider.AWS.value:
+                # vcpu ratio (assuming linear, intercept at 0 scaling)
+                # for aws lambda https://docs.aws.amazon.com/lambda/latest/dg/configuration-function-common.html
+                vcpu = self.get_memory(instance_name, provider_name) / 1792
+            else:
+                raise ValueError(
+                    f"vCPU count for instance {instance_name} in provider {provider_name} is not available"
+                )
+
+        return vcpu
+
+    def get_memory(self, instance_name: str, provider_name: str) -> float:
+        return (
+            self._instances_regions_and_providers.get(instance_name, {})
+            .get(provider_name, {})
+            .get("config", {})
+            .get("memory")
+        )  # Memory MUST exist for a valid workflow
+
+    def get_architecture(self, instance_name: str, provider_name: str) -> str:
+        return (
+            self._instances_regions_and_providers.get(instance_name, {})
+            .get(provider_name, {})
+            .get("config", {})
+            .get("architecture", SOLVER_INPUT_ARCHITECTURE_DEFAULT)
+        )  # Default to x86_64
+
+    def _retrieve_workflow_data(self, workflow_id: str) -> dict[str, Any]:
+        return self._retrieve_data(self._primary_table, workflow_id)
