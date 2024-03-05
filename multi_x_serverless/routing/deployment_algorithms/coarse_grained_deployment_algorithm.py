179af32a2f204f7fae3e05538b7ccf9eada089e0
vgsteiger <v.gsteiger@gmail.com>
2024-03-04 16:05:49 -0800

    Refactor deployment algorithm to use multiprocessing

--- multi_x_serverless/routing/deployment_algorithms/coarse_grained_deployment_algorithm.py
+++ multi_x_serverless/routing/deployment_algorithms/coarse_grained_deployment_algorithm.py
@@ -1,4 +1,6 @@
 from multi_x_serverless.routing.deployment_algorithms.deployment_algorithm import DeploymentAlgorithm
+from multiprocessing import Pool
+from typing import Optional
 
 
 class CoarseGrainedDeploymentAlgorithm(DeploymentAlgorithm):
@@ -7,21 +9,24 @@ class CoarseGrainedDeploymentAlgorithm(DeploymentAlgorithm):
         return deployments
 
     def _generate_all_possible_coarse_deployments(self) -> list[tuple[list[int], dict[str, float]]]:
-        deployments = []
-        for region_index in self._region_indexer.get_value_indices().values():
-            if any(
-                region_index not in self._per_instance_permitted_regions[instance_index]
-                for instance_index in range(self._number_of_instances)
-            ):
-                continue
-            deployment = self._generate_deployment(region_index)
-            deployment_metrics = self._deployment_metrics_calculator.calculate_deployment_metrics(deployment)
+        with Pool() as pool:
+            deployments = pool.map(self._generate_and_check_deployment, self._region_indexer.get_value_indices().values())
+        return [deployment for deployment in deployments if deployment is not None]
 
-            if self._is_hard_constraint_failed(deployment_metrics):
-                continue
+    def _generate_and_check_deployment(self, region_index: int) -> Optional[tuple[list[int], dict[str, float]]]:
 
-            deployments.append((deployment, deployment_metrics))
-        return deployments
+        if any(
+            region_index not in self._per_instance_permitted_regions[instance_index]
+            for instance_index in range(self._number_of_instances)
+        ):
+            return None
+        deployment = self._generate_deployment(region_index)
+        deployment_metrics = self._deployment_metrics_calculator.calculate_deployment_metrics(deployment)
+
+        if self._is_hard_constraint_failed(deployment_metrics):
+            return None
+
+        return (deployment, deployment_metrics)
 
     def _generate_deployment(self, region_index: int) -> list[int]:
         return [region_index for _ in range(self._number_of_instances)]
--- multi_x_serverless/routing/deployment_algorithms/fine_grained_deployment_algorithm.py
+++ multi_x_serverless/routing/deployment_algorithms/fine_grained_deployment_algorithm.py
@@ -1,4 +1,5 @@
 from itertools import product
+from multiprocessing import Pool
 
 from multi_x_serverless.routing.deployment_algorithms.deployment_algorithm import DeploymentAlgorithm
 
@@ -9,32 +10,30 @@ class FineGrainedDeploymentAlgorithm(DeploymentAlgorithm):
         return deployments
 
     def _generate_all_possible_fine_deployments(self) -> list[tuple[list[int], dict[str, float]]]:
-        deployments: list[tuple[list[int], dict[str, float]]] = []
+        with Pool() as pool:
+            all_combinations = product(
+                self._region_indexer.get_value_indices().values(),
+                repeat=self._number_of_instances,
+            )
+            deployments = pool.map(self._generate_and_check_deployment, all_combinations)
+        return [deployment for deployment in deployments if deployment is not None]
 
-        # Generate every possible combination of region and instances
-        all_combinations = product(
-            self._region_indexer.get_value_indices().values(),
-            repeat=self._number_of_instances,
+    def _generate_and_check_deployment(self, deployment_tuple: tuple[int, ...]) -> list[int]:
+        if any(
+            deployment_tuple[instance] not in self._per_instance_permitted_regions[instance]
+            for instance in range(self._number_of_instances)
+        ):
+            return None
+
+        # Type matching
+        deployment: list[int] = list(deployment_tuple)
+
+        # Calculate the deployment metrics for the mapping
+        deployment_metrics: dict[str, float] = self._deployment_metrics_calculator.calculate_deployment_metrics(
+            deployment
         )
 
-        for deployment_tuple in all_combinations:
-            if any(
-                deployment_tuple[instance] not in self._per_instance_permitted_regions[instance]
-                for instance in range(self._number_of_instances)
-            ):
-                continue
+        if self._is_hard_constraint_failed(deployment_metrics):
+            return None
 
-            # Type matching
-            deployment: list[int] = list(deployment_tuple)
-
-            # Calculate the deployment metrics for the mapping
-            deployment_metrics: dict[str, float] = self._deployment_metrics_calculator.calculate_deployment_metrics(
-                deployment
-            )
-
-            if self._is_hard_constraint_failed(deployment_metrics):
-                continue
-
-            deployments.append((deployment, deployment_metrics))
-
-        return deployments
+        return (deployment, deployment_metrics)
