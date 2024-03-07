cead778c4b9e6b1cccbac82516bfc67e89235882
Danidite <1099335751qq@gmail.com>
2024-03-07 15:23:49 -0800

    adjusted carbon_loader to conform to new format

--- multi_x_serverless/routing/deployment_input/components/loaders/carbon_loader.py
+++ multi_x_serverless/routing/deployment_input/components/loaders/carbon_loader.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Optional
 
 from multi_x_serverless.common.constants import (
     CARBON_REGION_TABLE,
@@ -18,10 +18,23 @@ class CarbonLoader(InputLoader):
     def setup(self, available_regions: set[str]) -> None:
         self._carbon_data = self._retrieve_region_data(available_regions)
 
-    def get_transmission_carbon_intensity(self, from_region_name: str, to_region_name: str) -> tuple[float, float]:
+    def get_transmission_carbon_intensity(self, from_region_name: str, to_region_name: str, hour: Optional[str] = None) -> tuple[float, float]:
+        carbon_policy = "hourly_average" if hour is not None else "overall_average"
+        policy_specific_data = self._carbon_data.get(from_region_name, {}).get(carbon_policy, {})
+        if hour is not None:
+            return (
+                policy_specific_data.get(hour, {})
+                .get("transmission_carbon", {})
+                .get(to_region_name, {})
+                .get("carbon_intensity", SOLVER_INPUT_TRANSMISSION_CARBON_DEFAULT),
+                self._carbon_data.get(from_region_name, {})
+                .get("transmission_carbon", {})
+                .get(to_region_name, {})
+                .get("distance", SOLVER_INPUT_TRANSMISSION_CARBON_DEFAULT)
+            )
+
         return (
-            self._carbon_data.get(from_region_name, {})
-            .get("transmission_carbon", {})
+            policy_specific_data.get("transmission_carbon", {})
             .get(to_region_name, {})
             .get("carbon_intensity", SOLVER_INPUT_TRANSMISSION_CARBON_DEFAULT),
             self._carbon_data.get(from_region_name, {})
@@ -30,5 +43,15 @@ class CarbonLoader(InputLoader):
             .get("distance", SOLVER_INPUT_TRANSMISSION_CARBON_DEFAULT),
         )
 
-    def get_grid_carbon_intensity(self, region_name: str) -> float:
-        return self._carbon_data.get(region_name, {}).get("carbon_intensity", SOLVER_INPUT_GRID_CARBON_DEFAULT)
+
+    def get_grid_carbon_intensity(self, region_name: str, hour: Optional[str] = None) -> float:
+        carbon_policy = "hourly_average" if hour is not None else "overall_average"
+        policy_specific_data = self._carbon_data.get(region_name, {}).get(carbon_policy, {})
+        if hour is not None:
+            return (
+                policy_specific_data.get(hour, {}).get("carbon_intensity", SOLVER_INPUT_GRID_CARBON_DEFAULT)
+            )
+
+        return (
+            policy_specific_data.get("carbon_intensity", SOLVER_INPUT_GRID_CARBON_DEFAULT)
+        )
