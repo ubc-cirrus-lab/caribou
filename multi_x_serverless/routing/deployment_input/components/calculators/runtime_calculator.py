6848c753bd4d02e4325c3504d7644347d2310b38
vgsteiger <v.gsteiger@gmail.com>
2024-03-08 14:10:09 -0800

    Remove commented out code in runtime_calculator.py and cost_calculator.py

--- multi_x_serverless/routing/deployment_input/components/calculators/runtime_calculator.py
+++ multi_x_serverless/routing/deployment_input/components/calculators/runtime_calculator.py
@@ -54,28 +54,3 @@ class RuntimeCalculator(InputCalculator):
         runtime_distribution.sort()
 
         return runtime_distribution
-
-    # def calculate_latency_distribution(
-    #     self, from_instance_name: str, to_instance_name: str, from_region_name: str, to_region_name: str
-    # ) -> np.ndarray:
-    #     raw_latency_distribution: list[float] = self._workflow_loader.get_latency_distribution(
-    #         from_instance_name, to_instance_name, from_region_name, to_region_name
-    #     )
-
-    #     # If the latency is not found, then we need to use the performance loader to estimate the relative latency
-    #     if len(raw_latency_distribution) == 0:
-    #         # TODO (#166): Potentially we can do something here? Take the home region latency?
-    #         if from_instance_name == "start_hop":
-    #             return np.array([0.0])
-    #         # Currently our performance loader does not consider data transfer size for latency
-    #         raw_latency_distribution = self._performance_loader.get_transmission_latency_distribution(
-    #             from_region_name, to_region_name
-    #         )
-
-    #     # Now we convert the list to a numpy array an return it
-    #     latency_distribution = np.array(raw_latency_distribution)
-
-    #     # Sort the array in place
-    #     latency_distribution.sort()
-
-    #     return latency_distribution
