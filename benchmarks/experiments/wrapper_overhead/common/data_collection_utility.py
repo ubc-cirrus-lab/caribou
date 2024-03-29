3a24132d727db9a56d6d8087a94fd739833a2163
Danidite <1099335751qq@gmail.com>
2024-03-29 10:49:03 -0700

    Refactor data_collection_utility.py and remove unused code

--- benchmarks/experiments/wrapper_overhead/common/data_collection_utility.py
+++ benchmarks/experiments/wrapper_overhead/common/data_collection_utility.py
@@ -215,27 +215,30 @@ class WrapperOverheadCollectionUtility():
                 time_results.append(max_time_from_invocation)
             else:
                 time_results.append(max_time_from_first_function)
-    
+
+        # Log the unique cpu models
+        print(f"Unique CPU Models: {unique_cpu_models}")
+
         return time_results
 
 
-if __name__ == "__main__":
-    desired_region = 'ca-west-1'
-    collection_utility = WrapperOverheadCollectionUtility(desired_region)
-    current_path = os.getcwd()
-    allowed_experiment_types = {
-        'boto3_direct': 'Direct Calls',
-        'boto3_sns': 'SNS Calls',
-        'multi_x': 'EntsGuard',
-        'aws_step_function': 'AWS Step Function',
-    }
+# if __name__ == "__main__":
+#     desired_region = 'ca-west-1'
+#     collection_utility = WrapperOverheadCollectionUtility(desired_region)
+#     current_path = os.getcwd()
+#     allowed_experiment_types = {
+#         'boto3_direct': 'Direct Calls',
+#         'boto3_sns': 'SNS Calls',
+#         'multi_x': 'EntsGuard',
+#         'aws_step_function': 'AWS Step Function',
+#     }
 
-    # Direct calls
-    experiment_paths = ['benchmarks/experiments/wrapper_overhead/dna_visualization/external_database']
-    full_experiment_paths = []
-    for experiment_path in experiment_paths:
-        full_path = os.path.join(current_path, experiment_path)
-        full_experiment_paths.append(full_path)
+#     # Direct calls
+#     experiment_paths = ['benchmarks/experiments/wrapper_overhead/dna_visualization/external_database']
+#     full_experiment_paths = []
+#     for experiment_path in experiment_paths:
+#         full_path = os.path.join(current_path, experiment_path)
+#         full_experiment_paths.append(full_path)
         
-    experimental_results = collection_utility.aquire_experimental_results(full_experiment_paths, allowed_experiment_types)
-    print(experimental_results)
+#     experimental_results = collection_utility.aquire_experimental_results(full_experiment_paths, allowed_experiment_types)
+#     print(experimental_results)
