5f34433a80b1833ae1e1510b831f8a647eb72eb7
Danidite <1099335751qq@gmail.com>
2024-07-31 20:25:32 +0000

    Adjusted retriever changes.

--- caribou/syncers/log_sync_workflow.py
+++ caribou/syncers/log_sync_workflow.py
@@ -81,8 +81,8 @@ class LogSyncWorkflow:  # pylint: disable=too-many-instance-attributes
         data_for_upload: str = self._prepare_data_for_upload(self._previous_data)
         self._upload_data(data_for_upload)
 
-        # TODO: Remove this print statement
-        print(json.dumps(json.loads(data_for_upload), indent=4))
+        # # TODO: Remove this print statement
+        # print(json.dumps(json.loads(data_for_upload), indent=4))
 
     def _upload_data(self, data_for_upload: str) -> None:
         self._workflow_summary_client.update_value_in_table(
