e0b8c26c3c7cb7ce900ab342df140790cd73e27c
pjavanrood <Parshan.pjavanrood@gmail.com>
2024-08-17 16:08:26 +0300

    removed lambda ml

--- benchmarks/lambda_ml/src/archived/elasticache/Memcached/clear_all.py
+++
@@ -1,5 +0,0 @@
-import memcache
-
-def clear_all(client):
-    client.flust_all()
-    return True
--- benchmarks/lambda_ml/src/archived/sync/sync_meta.py
+++
@@ -1,9 +0,0 @@
-
-class SyncMeta:
-
-    def __init__(self, worker_index, num_workers):
-        self.worker_index = worker_index
-        self.num_workers = num_workers
-
-    def __str__(self):
-        return "worker id = {}, total number of workers = {}".format(self.worker_index, self.num_workers)
