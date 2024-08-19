a4467563a61e6e925106ad3c6a41eb16caa79a26
Danidite <1099335751qq@gmail.com>
2024-08-19 15:40:02 -0700

    wip test deploy scripts.

---
+++ caribou/deployment/client/cli/aws_lambda_cli/aws_handler.py
@@ -0,0 +1,10 @@
+# Create a caribou handler
+import json
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
